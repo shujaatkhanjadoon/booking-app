fbb7a1060764aeccd9c98467f9c2ac211cadb15a
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-01-17 12:04:50 +0000

    commit

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -19,7 +19,7 @@ router.post("/register", async (req: Request, res: Response) => {
 
         const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "1d"});
 
-        res.cookie("auth-token", token, { 
+        res.cookie("auth_token", token, { 
             httpOnly: true, 
             secure: process.env.NODE === "production",
             maxAge: 86400000,
