c5440dd040f9faa7dee117d9c0eaaabc9857b98a
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-01-18 07:37:08 +0000

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import "dotenv/config";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 import { connectDB } from "./utills/features";
 
 // mongoose.connect(process.env.MONGODB_CONECTION_STRING as string);
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(urlencoded({extended: true}));
 app.use(cors());
 
-
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,18 +1,28 @@
 import { Request, Response, Router} from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {
+router.post("/register", [
+    check("email", "Email is required" ).isString(),
+    check("password", "Password with 6 or more characters is required").isLength({ min: 6 }),
+    check("firstName", "First name is required").isString(),
+    check("lastName", "Last name is required").isString(),
+], async (req: Request, res: Response) => {
 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message: errors.array()});
+    }
     try {
         let user = await User.findOne({
             email: req.body.email,
         });
 
         if(user){
-            return res.status(400).json({message: "Email already exist, try with new email"});
+            return res.status(400).json({message: "Email already exist, Please try with new one"});
         }
         user = new User(req.body);
         await user.save();
@@ -21,7 +31,7 @@ router.post("/register", async (req: Request, res: Response) => {
 
         res.cookie("auth_token", token, { 
             httpOnly: true, 
-            secure: process.env.NODE === "production",
+            secure: process.env.NODE_ENV === "production",
             maxAge: 86400000,
         });
 
